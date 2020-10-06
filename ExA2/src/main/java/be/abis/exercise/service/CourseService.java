1ce73f922a6b01dbd9c1b2fc5e7f425aa4db26db
bvanrooy <bart.van.rooy@gmail.com>
2020-10-06 14:03:19 +0200

    exercise A2

---
+++ ExA2/src/main/java/be/abis/exercise/service/CourseService.java
@@ -0,0 +1,12 @@
+package be.abis.exercise.service;
+
+import java.util.List;
+
+import be.abis.exercise.model.Course;
+
+public interface CourseService {
+
+	public List<Course> findAllCourses();
+	public Course findCourse(int id);
+	public Course findCours(String shortTitle);
+}
